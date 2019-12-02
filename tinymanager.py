7ba2e3e86aebeee6157dac6090314f4dc66f4f77
Blake Hannaford <blake.hannaford@gmail.com>
2019-12-02 13:30:46 -0800

    now runs as a test script

--- tinymanager.py
+++ tinymanager.py
@@ -45,7 +45,7 @@ class tdb_file():
             
         
         
-        
+    # generate schema based on existing tdb_file.
     def auto_schema(self):  # collect a schema from first document(s) in the db file
         #
         #  Note: this is basically a convenience hack -- assuming the first record
@@ -383,58 +383,59 @@ if __name__ == '__main__':
     #     the validator, then be fixable, then
     #     pass the validator.
     #
-    #
-     
-  
+    #  
+    os.system('python3 maketestdb.py')  # initialize the dbs
     dbfn_list = []
     db_or_table_list = []
     
-    dbfnamelist = sys.argv[1:]   # list of json files.
-    
     #expand all dbs into tables (if they have them)
-   
+
     dbfn_list = ['test1.json', 'test2.json', 'test3.json', 'test4.json', 'test5.json', ]
         
         
     for fn in dbfn_list:
         dbf = tdb_file(fn)
         print ('Running test: '+dbf.name)
-        result = 'Pass'        
         #following logic unpacks tinydb's that have non-default tables
-        print ('looking at: ', dbf.name)
         if dbf.db is not None:
+            dbf.auto_schema()  # get schema just once
             if len(dbf.tablelist) == 0:
                 db_or_table_list.append([dbf, None, dbf.name])
             else:
                 for table in dbf.tablelist:
                     db_or_table_list.append([dbf, table, dbf.name])
-
-        print('list: ', db_or_table_list)
-        print('\n\n\n')
-        report = []
-        report.append('\n\n  Testing report: ')
-        # now go through all tables
-        for item in db_or_table_list:
-            dbf = item[0]
-            table = item[1]
-            dname = item[2]
-            dbf.auto_schema()  # get schema
-            dbparent = dbf.db # the db itself
-            if table : # if there is a table
-                db = dbparent.table(table)
-            else: 
-                db = dbparent.table('_default')
-                
-            v = tdb_validator(dbf,table)
-            if v.valid_TF() == True:
+        else:
+            print('couldnt open '+fn)
+    print('list: ', db_or_table_list)
+    report = []
+    report.append('\n\n  Testing report: \n')
+    # now go through all tables
+    for item in db_or_table_list:
+        result = 'Pass'        
+        dbf = item[0]
+        table = item[1]
+        dname = item[2]
+        print ('testing: ',dname, table)
+        dbparent = dbf.db # the db itself
+        if table : # if there is a table
+            db = dbparent.table(table)
+        else: 
+            db = dbparent.table('_default')
+            
+        v = tdb_validator(dbf,table)
+        if v.valid_TF() == True:
+            if dbf.name != 'test1.json':  # test1.json is made to be valid!
                 result = 'Fail - invalid db tested good:'
-                continue
+            else:
+                result = 'Pass'  
+        else:
             v.repair_uniformity()
-            if v.valid_TF() == False:
-                result = 'Fail - repair failed.'
-            report.append('      result: '+result+'    '+dname+'   '+table)
+        if v.valid_TF() == False:
+            result = 'Fail - repair failed.'
+        report.append('    '+dname+'   '+table+'      result: '+result+'\n')
 
-    print(report)
+    for line in report:
+        print(line)
        
           
         
